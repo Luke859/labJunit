65a66f5f022ba67957f246d3f46cdf87ce5d9c0e
Luke <lukebryan@orange.fr>
2024-04-19 11:00:12 +0200

    Calculatrice ajout somme et produit

--- src/main/java/labJuinit/app/Calculatrice.java
+++ src/main/java/labJuinit/app/Calculatrice.java
@@ -5,4 +5,12 @@ public class Calculatrice {
     public Calculatrice() {
         System.out.println("Bienvenue à ma Calculatrice");
     }
+
+    public double somme(double a, double b) {
+        return a + b;
+    }
+
+    public double produit(double a, double b) {
+        return a * b;
+    }
 }
--- src/main/java/labJuinit/app/Main.java
+++ src/main/java/labJuinit/app/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main( String[] args )
     {
-
         Calculatrice c = new Calculatrice();
+
+        double s = c.somme(10, 20);
+        System.out.println("Le somme de 10 + 20 = " + s);
+
+        double p = c.produit(10, 20);
+        System.out.println("Le produit de 10 fois 20 = " + p);
     }
 }
